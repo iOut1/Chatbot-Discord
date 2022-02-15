200fe6475f5caae30183d355357cf30e94f99fd3
iOut1 <63367424+iOut1@users.noreply.github.com>
2022-02-15 13:23:43 -0800

    Fixed function error.

--- Chatbot.py
+++ Chatbot.py
@@ -7,6 +7,6 @@ chatbot = ChatBot('Communicator')
 trainer = ChatterBotCorpusTrainer(chatbot)
 trainer.train("chatterbot.corpus.english")
 
-def Response():
+def Response(Inputs):
   Reply = chatbot.get_response(Inputs)
   return Reply
