74e90303c22a6ed16645b22f842f40124883380b
odwkgorepkgprkv <63367424+iOut1@users.noreply.github.com>
2022-02-15 21:52:38 +0000

    Debugged indents.

--- Messages.py
+++ Messages.py
@@ -9,10 +9,10 @@ client = discord.Client()
 async def on_ready():
   print("We have logged in as {0.user}".format(client))
  
-@client.event():
-  async def on_message(message):
-    if message.author == client.user:
-      return
-    else:
-      await message.channel.send(Response(message))
+@client.event()
+async def on_message(message):
+  if message.author == client.user:
+    return
+  else:
+    await message.channel.send(Response(message))
      
