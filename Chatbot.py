f7d50b8d05d8d78802961c4152586caff96a6b6c
David Benshachar <63367424+iOut1@users.noreply.github.com>
2022-02-17 14:35:29 -0800

    Created comments.

--- Chatbot.py
+++ Chatbot.py
@@ -1,11 +1,12 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
-
+#Creates a chatbot but and trains it in English#
 chatbot = ChatBot('Communicator')
 trainer = ChatterBotCorpusTrainer(chatbot)
 trainer.train("chatterbot.corpus.english")
 
+#Creates a function to truly communicate with chatbot#
 def Response(Inputs):
   Reply = chatbot.get_response(Inputs)
   return Reply
