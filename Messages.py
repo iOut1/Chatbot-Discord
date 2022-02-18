b4dc944dcc36b8cdb4627754192f2165757e9d88
David Benshachar <63367424+iOut1@users.noreply.github.com>
2022-02-17 18:13:07 -0800

    Added comments.

--- Messages.py
+++ Messages.py
@@ -5,10 +5,12 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 
 client = discord.Client()
 
+# Tells that it the bot has signed in#
 @client.event()
 async def on_ready():
   print("We have logged in as {0.user}".format(client))
- 
+
+# Checks newest message, checks if that message was sent by it. Then makes the chatbot send a response#
 @client.event()
 async def on_message(message):
   if message.author == client.user:
